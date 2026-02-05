c15af681602417ea2dda0b1d934d09f8cd94cb7e
Onise <tsotskhalashvili558@gmail.com>
2026-02-05 21:24:17 +0400

    Add Conection string

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,9 +8,21 @@ namespace BookSystem.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            var connString = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            if (string.IsNullOrEmpty(connString))
+            {
+                // 1. აქ ჩასვი ის DATABASE_PUBLIC_URL, რომელიც დააკოპირე
+                // 2. ბოლოში მიაწერე ?sslmode=require
+                optionsBuilder.UseNpgsql("postgresql://postgres:sZMHaECUNouZutVpeolnrgQwlJIKzRHB@yamabiko.proxy.rlwy.net:33961/railway");
+            }
+            else
+            {
+                // Railway-ზე გაშვებისას გამოიყენებს შიდა DATABASE_URL-ს
+                optionsBuilder.UseNpgsql(connString);
+            }
         }
-       
+
         public DbSet<Folder> Folders { get; set; }  
         public DbSet<Book> books { get; set; }  
 
