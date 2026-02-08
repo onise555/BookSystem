ce5dc9f1f390d430e065d7b833b91ce41be465a7
Onise <tsotskhalashvili558@gmail.com>
2026-02-08 16:45:05 +0400

    Add BookDetail Post Method

---
+++ Controllers/BookDetailController.cs
@@ -0,0 +1,36 @@
+﻿using BookSystem.Data;
+using BookSystem.Models;
+using BookSystem.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookDetailController : ControllerBase
+    {
+        private readonly DataContext _data;
+
+        public  BookDetailController(DataContext data)
+        {
+            _data = data;   
+        }
+
+        [HttpPost("Add-Book-Detail")]
+        public ActionResult AddBookDetail(CreateBookDetailRequest req)
+        {
+            var detail = new BookDetail
+            {
+                Description = req.Description,
+                Author = req.Author,
+                BookId = req.BookId,
+            };
+
+            _data.details.Add(detail);  
+            _data.SaveChanges();    
+
+            return Ok(detail);  
+        }
+    }
+}
---
+++ Requests/CreateBookDetailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BookSystem.Requests
+{
+    public class CreateBookDetailRequest
+    {
+        public string? Description { get; set; }
+
+        public string? Author { get; set; }
+
+        public int BookId { get; set; }
+    }
+}
